51d2617a9dc8e38235efee7f1c188b5891f9c202
yuriykolisnyk <ykolisnyk@gmail.com>
2021-09-29 12:57:40 +0300

    Update

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Иморт по дефолту (когда один елемент)
+// import qwe from "./js/validate.js";
+// console.log(qwe);
+
+// Ипорт отдельных елементов
+// import { fetchAllUsers, fetchUsersById } from "./js/api-service";
+// console.log(fetchAllUsers);
+// console.log(fetchUsersById);
+
+// Иморт всех елементов
+import * as apiService from "./js/api-service";
+console.log(apiService);
---
+++ src/js/api-service.js
@@ -0,0 +1,10 @@
+export const fetchAllUsers = () => {
+  console.log("fetchAllUsers");
+};
+export const fetchUsersById = (id) => {
+  console.log("fetchUsersById");
+};
+export const updateUsersById = (id) => {
+  console.log("updateUsersById");
+};
+export const x = 5;
